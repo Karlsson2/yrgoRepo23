67c497bdc13ec4f198b6a12f45a4f9652c199c09
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2023-09-15 10:42:35 +0200

    lesson 1 php completed

---
+++ php/Lesson03/01.php
@@ -0,0 +1,14 @@
+
+
+<?php   
+/* Remember the last lesson where we talked about variables and strings? Let's recap! 
+Create a new PHP file and name it 01.php. Define the variables $name and $numberOfFamilies 
+and assign the values William and 2. Use the variables to create the sentence below. The output should look like the text below.
+
+William is part of 2 families. */
+    $name = "William";
+    $numberOfFamilies = 2;
+
+    echo "$name is part of $numberOfFamilies families";
+
+?>
