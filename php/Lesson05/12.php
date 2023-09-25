d31043239fc0bbbdec2f80b749e0de270ab360ac
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2023-09-25 13:09:17 +0200

    updated assignment 12

--- php/Lesson05/12.php
+++ php/Lesson05/12.php
@@ -4,15 +4,21 @@
     $suits = ["Hearts","Spades","Diamonds","Clubs"];
 
     foreach($suits as $suit){
+        $color = "";
+        if ($suit == "Hearts" || $suit == "Diamonds"){
+            $color="red";
+        }else {
+            $color="black";
+        }
         for ($i = 0; $i <=13; $i++){
-            $deckOfCards[] = ["suit" => $suit, "rank" => $i];
+            $deckOfCards[] = ["suit" => $suit, "rank" => $i+1, "color" => $color];
         };
     };
     
 
 
     foreach($deckOfCards as $cards){
-        echo "suit is: " . $cards["suit"] . " rank is: " . $cards["rank"] . "</br>";
+        echo "suit is: " . $cards["suit"] . " rank is: " . $cards["rank"] .  " color is: " . $cards["color"] . "</br>";
     }
 
 ?>
